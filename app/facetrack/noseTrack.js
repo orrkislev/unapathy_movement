a5224497469d05ed995942470cc5f3dcb68db864
Orr Kislev <orrkislev@gmail.com>
2020-08-02 09:32:29 +0300

    fix nose

--- app/facetrack/noseTrack.js
+++ app/facetrack/noseTrack.js
@@ -2,9 +2,10 @@ let prevNosePosition = null;
 
 function updateNose(nosePosition) {
   if (nosePosition == null) return null
-  if (prevNosePosition) {
-    const noseSpeed = dist(nosePosition[0], nosePosition[1], prevNosePosition[0], prevNosePosition[1])
-    return noseSpeed
-  }
+
+  let noseSpeed = null
+  if (prevNosePosition) 
+    noseSpeed = dist(nosePosition[0], nosePosition[1], prevNosePosition[0], prevNosePosition[1])
   prevNosePosition = nosePosition;
+  return noseSpeed
 }
